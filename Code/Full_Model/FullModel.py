Major add ons:  - trees pop up if no agriculture and no trees - Population Max is now gaussian - treePref is now tanh (fixed a small scaling bug)

Code/Full_Model/FullModel.py
@@ -39,7 +39,7 @@ from observefunc import observe, observe_density, plot_movingProb
 from agents import agent, init_agents#, init_agents_EI
 #from agents import Tree
 from update import update_single_agent, update_time_step
-from LogisticRegrowth import regrow_update, agric_update  # EasterIslands/Code/TreeRegrowth_spatExt/")
+from LogisticRegrowth import regrow_update, agric_update, popuptrees  # EasterIslands/Code/TreeRegrowth_spatExt/")
 
 from write_to_ncdf import final_saving#,  produce_agents_DataFrame
 #from analyse_ncdf import plot_statistics
@@ -60,10 +60,10 @@ config.index_count=0
 
 ### RUN
 config.updatewithreplacement = False
-config.StartTime = 1200
-config.EndTime=1500
+config.StartTime = 800
+config.EndTime=1850
 config.N_timesteps = config.EndTime-config.StartTime
-config.seed= 8
+config.seed= 12
 #config.folder= LATER
 
 ### HOUSEHOLD PARAMS
@@ -72,7 +72,12 @@ config.MaxSettlementSlope=6
 #config.MinSettlementElev=10
 config.SweetPointSettlementElev = 0
 
-config.max_pop_per_household = 50
+config.max_pop_per_household_mean = 3.5*12  # Bahn2017 2-3 houses with each a dozen people. But I assume they also should include children
+config.max_pop_per_household_std = 0.5*12 
+
+
+
+LowerLimit_PopInHousehold = 10
 
 config.MaxPopulationDensity=173*3  #500 # DIamond says 90-450 ppl/mile^2  i.e. 34 to 173ppl/km^2 BUT THIS WILL BE HIGHER IN CENTRES OF COURSE
 #config.MaxAgricPenalty = 100
@@ -80,13 +85,18 @@ config.MaxPopulationDensity=173*3  #500 # DIamond says 90-450 ppl/mile^2  i.e. 3
 config.tree_need_per_capita = 5 # Brandt Merico 2015 h_t =5 roughly.
 config.MinTreeNeed=0.3      #Fraction
 
-config.BestTreeNr_forNewSpot = 10*config.max_pop_per_household*config.tree_need_per_capita #HowManyTreesAnAgentWantsInARadiusWhenItMoves = 
+config.BestTreeNr_forNewSpot = 10*(config.max_pop_per_household_mean+config.max_pop_per_household_std)*config.tree_need_per_capita #HowManyTreesAnAgentWantsInARadiusWhenItMoves = 
+
+
 
 #config.Nr_AgricStages = 10
 #config.dStage = ((1-config.MinTreeNeed)/config.Nr_AgricStages)
 
 #config.agricSites_need_per_Capita = 2./6.  # Flenley Bahn
 config.agricYield_need_per_Capita = 0.5  # Puleston High N  in ACRE!
+
+config.maxNeededAgric = np.ceil((config.max_pop_per_household_mean+config.max_pop_per_household_std)*config.agricYield_need_per_Capita)
+
 
 #YearsOfDecreasingTreePref = 1400-config.StartTime #400
 #config.treePref_decrease_per_year = (config.init_TreePreference - config.MinTreeNeed)/YearsOfDecreasingTreePref #0.002
@@ -99,7 +109,7 @@ config.agricYield_need_per_Capita = 0.5  # Puleston High N  in ACRE!
 config.params={'tree_search_radius': 1.6, 
         'agriculture_radius':0.8,
         'moving_radius': 8,
-        'reproduction_rate': 0.02,  # logistically Max from BrandtMerico2015 # 0.007 from Bahn Flenley
+        'reproduction_rate': 0.007,  # logistically Max from BrandtMerico2015 # 0.007 from Bahn Flenley
         }
 
 # DEFAULT RUN
@@ -128,9 +138,12 @@ config.TreeDensityConditionParams = {
     'maxElev': 430,'maxSlope':8.5,
     "factorBetweenHighandLowTreeDensity":2}
 
+config.tree_pop_percentage = 0.01
+config.tree_pop_timespan  = 5
+
 config.UpperLandSoilQuality=0.2
 config.ErodedSoilYield=0.7
-config.YearsBeforeErosionDegradation=20
+#config.YearsBeforeErosionDegradation=20
 # need to make sure that 80% (RULL) are covered
 #config.AgriConds={'minElev':20,'maxElev_highQu':250,
 #    'maxSlope_highQu':3.5,'maxElev_lowQu':380,'maxSlope_lowQu':6,
@@ -147,7 +160,7 @@ config.tree_regrowth_rate=0.05 # every 20 years all trees are back?  # Brandt Me
 #####    Load or Create MAP     ##############
 ##############################################
 print("################   LOAD / CREATE MAP ###############")
-NewMap=False
+NewMap=True
 
 #if NewMap==False and (not config.N_init_trees==12e6 or not config.):
 #   print("Probably you should create a new Map!!")
@@ -181,7 +194,7 @@ print("################   DONE: MAP ###############")
 ########   Folder and Run Prepa   ###############
 #################################################
 
-config.folder = "Figs/Test/"
+config.folder = "Figs/"
 config.folder += "FullModel_grid"+str(config.gridpoints_y)+"_repr"+'%.0e' % (config.params['reproduction_rate'])+"_mv"+"%.0f" % config.params['moving_radius']+"_Standard/"#"FullModel_"+config.agent_type+"_"+config.map_type+"_"+config.init_option+"/"
 config.analysisOn=True
 #string = [item+r":   "+str(vars(config)[item]) for item in dir(config) if not item.startswith("__")]
@@ -214,7 +227,10 @@ def run():
 
         update_time_step(t)  
         regrow_update(config.tree_regrowth_rate)
+        agric_update()
+        popuptrees(t)
         #observe(t+1)
+
         if len(config.agents)>0:
 
             ag = config.agents[0]

Code/Full_Model/LogisticRegrowth.py
@@ -26,16 +26,28 @@ def regrow_update(g0):
     return 
 
 def agric_update():
-    notreecells = np.where(config.EI.tree_density==0)[0]
-    config.EI.treeless_years[notreecells]+=1
+    notreecells = np.where(config.EI.tree_density==0)[0].astype(int)
+    #config.EI.treeless_years[notreecells]+=1
     # For all cells with 20 years without trees, decrease soil quality to Eroded Soil yield or even lower
-    config.EI.agric_yield[notreecells] =  np.array([np.min(config.EI.agric_yield[i], config.ErodedSoilYield) if config.EI.treeless_years[i]>config.YearsBeforeErosionDegradation else config.EI.agric_yield[i] for i in notreecells])
+    #if config.EI.treeless_years[i]>config.YearsBeforeErosionDegradation else config.EI.agric_yield[i] 
+    config.EI.agric_yield[notreecells] =  np.array([np.minimum(config.EI.agric_yield[i], config.ErodedSoilYield) for i in notreecells])
     #for n,triang in enumerate(config.EI.EI_triangles):
     #    if 
     return 
 
-def popuptrees():
-    # SET treeless_years to 0
+def popuptrees(t):
+    if t>config.StartTime+config.tree_pop_timespan+1:
+        #no_agric = np.where(config.EI.agriculture==0)
+        no_agric = np.where(config.Array_agriculture[:,-config.tree_pop_timespan:].sum(axis=1) == 0)[0].astype(int)
+
+        to_pop_up = np.where(config.EI.tree_density==0)[0].astype(int)
+        tree_growth_poss = np.where(config.EI.carrying_cap>0)[0].astype(int)
+        
+        inds, counts = np.unique(np.concatenate([no_agric, to_pop_up,tree_growth_poss]), return_counts=True)
+        
+        where_to_pop = inds[counts>2]
+
+        config.EI.tree_density[where_to_pop] = config.tree_pop_percentage * config.EI.carrying_cap[where_to_pop]
     return 
 
 

Code/Full_Model/agents.py
@@ -94,8 +94,9 @@ class agent:
         return 
 
     def calc_new_tree_pref(self):
-        self.treePref = 0.5*np.tanh(5*(config.EI.tree_density[self.mv_inds].sum()/config.EI.carrying_cap[self.mv_inds].sum() - 0.5))+ 0.5
+        treePref_g = 0.5*np.tanh(5*(config.EI.tree_density[self.mv_inds].sum()/config.EI.carrying_cap[self.mv_inds].sum() - 0.5))+ 0.5
         #logistic_g = 1-config.EI.tree_density[self.mv_inds].sum()/config.EI.carrying_cap[self.mv_inds].sum()
+        self.treePref = config.MinTreeNeed + (config.init_TreePreference-config.MinTreeNeed) * treePref_g
         return 
 
 
@@ -116,7 +117,7 @@ class agent:
         config.EI.populationOccupancy[self.triangle_ind]-= dead_pop
 
         # CHECK IF AGENT IS STILL LARGE ENOUGH OR DIES
-        if self.pop < config.init_pop:
+        if self.pop < config.LowerLimit_PopInHousehold:
             config.deaths+= self.pop
             config.EI.agentOccupancy -=1
             config.EI.populationOccupancy[self.triangle_ind] -= dead_pop
@@ -226,9 +227,7 @@ class agent:
         # OPTION 2 updated: Including low quality and eroded soil
         AvailAgrisYield_aroundSites = np.dot(config.EI.agric_yield*(config.EI.nr_highqualitysites + config.EI.nr_lowqualitysites - config.EI.agriculture), mvTris_inds_arr_agric)  
         
-        
-        maxNeededAgric = np.ceil(config.max_pop_per_household*config.agricYield_need_per_Capita)
-        agriculture_penalty = (1-self.treePref)/config.MinTreeNeed * (maxNeededAgric - AvailAgrisYield_aroundSites.clip(min=0, max=maxNeededAgric))/maxNeededAgric
+        agriculture_penalty = (1-self.treePref)/config.MinTreeNeed * (config.maxNeededAgric - AvailAgrisYield_aroundSites.clip(min=0, max=config.maxNeededAgric))/config.maxNeededAgric
 
         survivalCond_agric =  AvailAgrisYield_aroundSites>self.AgriNeed
 
@@ -385,7 +384,7 @@ class agent:
         config.EI.populationOccupancy[self.triangle_ind] +=newborns
 
         # IF Population TOO LARGE; SPlit AGENT
-        if self.pop>config.max_pop_per_household:
+        if self.pop> np.random.normal(config.max_pop_per_household_mean, config.max_pop_per_household_std):
             child = copy(self)
             child.index = config.index_count
             config.index_count+=1